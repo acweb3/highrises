28792e44b6937545fe3722e3e2340a7f656cedcb
Brian Morgan <95297785+brianwmorgan@users.noreply.github.com>
2023-01-22 16:18:55 -0500

    remove old shit

--- static-site/src/components/ExplorerV2/index.js
+++ static-site/src/components/ExplorerV2/index.js
@@ -1,31 +1,9 @@
 import { DesktopExplorer } from 'components/ExplorerV2/DesktopExplorer';
 import * as S from 'components/ExplorerV2/ExplorerV2.styled';
-import { Paragraph } from 'components/ui/Paragraph';
 
 export const ExplorerV2 = () => {
     return (
         <>
-            <div
-                className="Main-content"
-                css={`
-                    background: #fff;
-                    min-height: 96px;
-                `}
-            >
-                <S.ExplorerHeader>Explore all highrises</S.ExplorerHeader>
-                <Paragraph
-                    css={`
-                        text-indent: 0;
-                        max-width: 750px;
-                        margin-bottom: 16px !important;
-                    `}
-                >
-                    Flip through the collection of more than 50 historic
-                    American skyscrapers and learn how each one came to be. Sort
-                    by architectural style, city, decade, and more to find your
-                    favorites!
-                </Paragraph>
-            </div>
             <S.ExplorerV2
                 css={`
                     position: relative;
