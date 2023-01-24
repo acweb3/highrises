874eb8f475d1ba1810aa5c8180f3f3176a009d24
brianwmorgan <brianwmorgan317@gmail.com>
2023-01-24 17:58:50 -0500

    refactored Helmet

--- static-site/src/templates/About.js
+++ static-site/src/templates/About.js
@@ -1,49 +1,29 @@
-import { Collage } from 'components/Collage';
 import { Editing } from 'components/Editing';
 import { EmailCollection } from 'components/EmailCollection';
 import { FAQ } from 'components/FAQ';
 import { Intent } from 'components/Intent';
-import { NavTabs } from 'components/NavTabs';
 // import { Reserve } from 'components/Reserve';
+import { Metadata } from 'components/Metadata';
+import { NavTabs } from 'components/NavTabs';
 import { Roadmap } from 'components/Roadmap';
 import { SiteWrapper } from 'components/SiteWrapper';
 import { Team } from 'components/Team';
 import { Contexts } from 'contexts';
 import 'css/index.css';
-import Helmet from 'react-helmet';
 
 const App = ({ pageContext: { highrises, thumbnail } }) => {
     return (
         <Contexts highrises={highrises}>
             <SiteWrapper>
-                <Helmet>
-                    <title>Highrises - HYTHA.CG</title>
-                    <meta name="title" content="Highrises - HYTHA.CG" />
-                    <meta
-                        name="description"
-                        content="Highrises are the iconic elements of American cities. Reaching radical new heights in technological advancement, skyscrapers fused Classical, Renaissance, and Gothic motifs onto steel and defined a new architectural language with Art Deco and International."
-                    />
-                    <meta property="og:type" content="website" />
-                    <meta property="og:title" content="Highrises - HYTHA.CG" />
-                    <meta property="og:url" content="highrises.hythacg.com" />
-                    <meta
-                        property="og:site_name"
-                        content="highrises.hythacg.com"
-                    />
-                    <meta
-                        property="og:description"
-                        content="Highrises are the iconic elements of American cities. Reaching radical new heights in technological advancement, skyscrapers fused Classical, Renaissance, and Gothic motifs onto steel and defined a new architectural language with Art Deco and International."
-                    />
-                    <meta property="og:type" content="website" />
-                    <meta name="twitter:title" content="Highrises - HYTHA.CG" />
-                    <meta property="og:image" content={thumbnail} />
-                    <meta name="twitter:card" content="summary_large_image" />
-                    <meta name="twitter:creator" content="@Hythacg" />
-                    <meta name="twitter:site" content="@Hythacg" />
-                    <meta property="twitter:image" content={thumbnail} />
-                    <meta property="twitter:image:alt" content="" />
-                    <link rel="canonical" href="highrises.hythacg.com" />
-                </Helmet>
+                <Metadata
+                    title={'Highrises - HYTHA.CG'}
+                    description={
+                        'Highrises are the iconic elements of American cities. Reaching radical new heights in technological advancement, skyscrapers fused Classical, Renaissance, and Gothic motifs onto steel and defined a new architectural language with Art Deco and International.'
+                    }
+                    ogUrl={'highrises.hythacg.com'}
+                    thumbnail={thumbnail}
+                    thumbnailAlt={''}
+                />
                 <NavTabs />
                 {/* <Reserve /> */}
                 {/* <Collage /> */}
@@ -51,7 +31,6 @@ const App = ({ pageContext: { highrises, thumbnail } }) => {
                 <Editing />
                 <Roadmap />
                 <Team />
-
                 <FAQ />
                 <EmailCollection />
             </SiteWrapper>
