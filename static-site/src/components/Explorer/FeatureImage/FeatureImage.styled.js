c7b3dccf380a80179f260db8d1b3a993084c2488
brianwmorgan <brianwmorgan317@gmail.com>
2023-01-26 21:04:59 -0500

    change feature image css

--- static-site/src/components/Explorer/FeatureImage/FeatureImage.styled.js
+++ static-site/src/components/Explorer/FeatureImage/FeatureImage.styled.js
@@ -3,11 +3,8 @@ import styled from 'styled-components';
 export const FeatureImage = styled.img`
     max-width: none;
     height: 100%;
-`;
-
-export const FeatureImagePicture = styled.div`
-    position: absolute;
-    height: 100%;
+    width: 100%;
+    object-fit: cover;
 `;
 
 export const FeatureImageWrapper = styled.div`
--- static-site/src/components/Explorer/FeatureImage/index.js
+++ static-site/src/components/Explorer/FeatureImage/index.js
@@ -4,9 +4,7 @@ import * as S from 'components/Explorer/FeatureImage/FeatureImage.styled';
 export const FeatureImage = () => {
     return (
         <S.FeatureImageWrapper>
-            <S.FeatureImagePicture>
-                <S.FeatureImage src={buildingImage} alt="building image" />
-            </S.FeatureImagePicture>
+            <S.FeatureImage src={buildingImage} alt="building image" />
         </S.FeatureImageWrapper>
     );
 };
